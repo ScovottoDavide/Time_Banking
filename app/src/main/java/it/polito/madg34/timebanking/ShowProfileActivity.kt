441d6ab7b339e2e09d7074f28e283eae70c3abb7
luciorocco <s289276@studenti.polito.it>
2022-03-30 19:56:45 +0100

    fix_3

--- app/src/main/java/it/polito/madg34/timebanking/ShowProfileActivity.kt
+++ app/src/main/java/it/polito/madg34/timebanking/ShowProfileActivity.kt
@@ -1,10 +1,18 @@
 package it.polito.madg34.timebanking
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.ActionBar
 import android.os.Bundle
+import android.util.TypedValue
+import android.view.Gravity
+import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.github.florent37.expansionpanel.ExpansionHeader
 import com.github.florent37.expansionpanel.ExpansionLayout
+import java.util.*
+
 
 class ShowProfileActivity : AppCompatActivity() {
 
@@ -12,7 +20,7 @@ class ShowProfileActivity : AppCompatActivity() {
     val nickname = "Draco123"
     val email = "mariorossi@general.it"
     val location = "Corso Castelfidardo, 39, Torio TO"
-    val skills: Array<String> = arrayOf("Dog sitter", "Chef")
+    val skills: Array<String> = arrayOf("Dog Sitter", "Chef")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,7 +40,49 @@ class ShowProfileActivity : AppCompatActivity() {
 
         /** Expand Skill to get description **/
         val layout = findViewById<ExpansionLayout>(R.id.expansionLayout)
+        val layoutExp = findViewById<LinearLayout>(R.id.expansionLinear)
         val arrow = findViewById<ImageView>(R.id.headerIndicator)
+
+        //val lastLinear = findViewById<LinearLayout>(R.id.lastLinear)
+
+        skills.forEachIndexed { index , element ->
+            var x = TextView(this)
+            x.text = "Skill${index+1}: ${element}\nDescription: Provide some description"
+            x.setTextSize(20F)
+            layoutExp.addView(x, index)
+        }
+
+
+
+       /* skills.forEach {
+
+            val newExHeader = ExpansionHeader(this)
+            val newExLayout = ExpansionLayout(this)
+            val image = ImageView(this)
+            val x = TextView(this)
+
+            image.setImageResource(com.github.florent37.expansionpanel.R.drawable.ic_expansion_header_indicator_grey_24dp)
+
+            x.text = it
+            x.setTextSize(20F)
+
+
+            lastLinear.addView(newExHeader)
+            lastLinear.addView(newExLayout)
+
+
+            newExHeader.addView(x)
+            newExHeader.addView(image)
+
+            image.setOnClickListener{
+                newExLayout.toggle(true)
+                if(newExLayout.isExpanded)
+                    image.rotation = 90F
+                else image.rotation = 0F
+            }
+
+        }*/
+
         arrow.setOnClickListener {
             layout.toggle(true)
             if(layout.isExpanded)
